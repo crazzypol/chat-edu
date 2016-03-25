7e4785dd9262acde13afc5160c5720770189c959
crazzypol <crazzypol@gmail.com>
2016-03-25 09:39:51 +0300

    fist

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ChatForLan
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lvUsers = new System.Windows.Forms.ListView();
+            this.rtReceiveMessage = new System.Windows.Forms.RichTextBox();
+            this.bnSendMessage = new System.Windows.Forms.Button();
+            this.chUsers = new System.Windows.Forms.ColumnHeader();
+            this.rtSendMessage = new System.Windows.Forms.RichTextBox();
+            this.panel2.SuspendLayout();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(577, 32);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.rtSendMessage);
+            this.panel2.Controls.Add(this.bnSendMessage);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 448);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(577, 66);
+            this.panel2.TabIndex = 2;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 32);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lvUsers);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.rtReceiveMessage);
+            this.splitContainer1.Size = new System.Drawing.Size(577, 416);
+            this.splitContainer1.SplitterDistance = 192;
+            this.splitContainer1.TabIndex = 3;
+            this.splitContainer1.SplitterMoved += new System.Windows.Forms.SplitterEventHandler(this.splitContainer1_SplitterMoved);
+            // 
+            // lvUsers
+            // 
+            this.lvUsers.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chUsers});
+            this.lvUsers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvUsers.FullRowSelect = true;
+            this.lvUsers.GridLines = true;
+            this.lvUsers.Location = new System.Drawing.Point(0, 0);
+            this.lvUsers.MultiSelect = false;
+            this.lvUsers.Name = "lvUsers";
+            this.lvUsers.Size = new System.Drawing.Size(192, 416);
+            this.lvUsers.TabIndex = 0;
+            this.lvUsers.UseCompatibleStateImageBehavior = false;
+            this.lvUsers.View = System.Windows.Forms.View.Details;
+            // 
+            // rtReceiveMessage
+            // 
+            this.rtReceiveMessage.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.rtReceiveMessage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtReceiveMessage.Location = new System.Drawing.Point(0, 0);
+            this.rtReceiveMessage.Name = "rtReceiveMessage";
+            this.rtReceiveMessage.Size = new System.Drawing.Size(381, 416);
+            this.rtReceiveMessage.TabIndex = 0;
+            this.rtReceiveMessage.Text = "";
+            // 
+            // bnSendMessage
+            // 
+            this.bnSendMessage.Location = new System.Drawing.Point(3, 3);
+            this.bnSendMessage.Name = "bnSendMessage";
+            this.bnSendMessage.Size = new System.Drawing.Size(84, 60);
+            this.bnSendMessage.TabIndex = 0;
+            this.bnSendMessage.Text = "Отправить сообщение";
+            this.bnSendMessage.UseVisualStyleBackColor = true;
+            this.bnSendMessage.Click += new System.EventHandler(this.bnSendMessage_Click);
+            // 
+            // chUsers
+            // 
+            this.chUsers.Text = "Пользователи";
+            this.chUsers.Width = 187;
+            // 
+            // rtSendMessage
+            // 
+            this.rtSendMessage.Location = new System.Drawing.Point(93, 3);
+            this.rtSendMessage.Name = "rtSendMessage";
+            this.rtSendMessage.Size = new System.Drawing.Size(481, 60);
+            this.rtSendMessage.TabIndex = 2;
+            this.rtSendMessage.Text = "";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 514);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "LanChat";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.panel2.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView lvUsers;
+        private System.Windows.Forms.RichTextBox rtReceiveMessage;
+        private System.Windows.Forms.Button bnSendMessage;
+        private System.Windows.Forms.ColumnHeader chUsers;
+        private System.Windows.Forms.RichTextBox rtSendMessage;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+using System.Threading;
+
+namespace ChatForLan
+{
+    public partial class frmMain : Form
+    {
+        const string MCASTADDR = "192.167.0.255";
+        const int MCASTPORT = 32760;
+        
+        ArrayList userList = new ArrayList();
+        bool IsThreadRunning = false;
+        
+        Thread _listener; 
+        delegate ChangeUserList();
+        event ChangeUserList _updateUserList;
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            _listener = new Thread(new ThreadStart(_ReceiveWorker));
+            _listener.Start();
+            String message = "Hello$" + GetName();
+            MulticastSend(MCASTADDR, MCASTPORT, message);
+        }
+        private void _ReceiveWorker() {
+            Receive(MCASTADDR, MCASTPORT);
+        }
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            lvUsers.Columns[0].Width = splitContainer1.Panel1.Width-4;
+        }
+
+        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+            lvUsers.Columns[0].Width = splitContainer1.Panel1.Width - 4;
+        }
+
+        private void bnSendMessage_Click(object sender, EventArgs e)
+        {
+            if (rtSendMessage.Text == "") return;
+            String _message = "MSG:$" + GetName() + " : " + rtSendMessage.Text;
+            if (lvUsers.SelectedItems.Count == 0) return;
+            string _user = lvUsers.SelectedItems[0].Text;
+            if (_user.Length > 1)
+            {
+                string _ipaddr;
+                IPAddress[] _ipaddrs = Dns.GetHostEntry(_user).AddressList;
+                if (_ipaddrs.Length > 1)
+                {
+                    _ipaddr = _ipaddrs[1].ToString();
+                }
+                else
+                {
+                    _ipaddr = _ipaddrs[0].ToString();
+                }
+                MulticastSend(_ipaddr, MCASTPORT, _message);
+                rtSendMessage.Text = "";
+            }
+        }
+
+        private void MulticastSend(string mAddress, int port, string message)
+        {
+            try
+            {
+                IPAddress GroupAddress = IPAddress.Parse(mAddress);
+                int GroupPort = port;
+                UdpClient sender = new UdpClient();
+                IPEndPoint groupEP = new IPEndPoint(GroupAddress, GroupPort);
+                byte[] bytes = Encoding.ASCII.GetBytes(message);
+                sender.Send(bytes, bytes.Length, groupEP);
+                sender.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private void Receive(string address, int port)
+        {
+            Socket _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint _ipendp = new IPEndPoint(IPAddress.Any, port);
+            _socket.Bind(_ipendp);
+            IPAddress _ipaddr = IPAddress.Parse(address);
+            //_socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(_ipaddr, IPAddress.Broadcast));
+            IsThreadRunning = true;
+            while (IsThreadRunning)
+            {
+                byte[] _receivebuf = new byte[500];
+                _socket.Receive(_receivebuf);
+                string _receivestr = System.Text.Encoding.ASCII.GetString(_receivebuf, 0, _receivebuf.Length);
+                _receivestr = _receivestr.Replace("\0", "");
+                if (_receivestr.StartsWith("Hello"))
+                {
+                    string[] array = _receivestr.Split('$');
+                    if (array.Length > 1)
+                    {
+                        if (!userList.Contains(array[1].ToString()))
+                        {
+                            userList.Add(array[1]);
+                            lvUsers.Items.Add(array[1]);
+                        }
+                    }
+                }
+                else if (_receivestr.StartsWith("Stop"))
+                {
+                    string[] array = _receivestr.Split('$');
+                    if (array.Length > 1)
+                    {
+                        if (userList.Contains(array[1].ToString()))
+                        {
+                            userList.Remove(array[1]);
+                            lvUsers.Items.RemoveByKey(array[1]);
+                        }
+                    }
+                }
+                else if (_receivestr.StartsWith("MSG:"))
+                {
+                    _receivestr = _receivestr.Replace("MSG:$", "");
+                    int start = this.rtReceiveMessage.TextLength;
+                    int length = _receivestr.IndexOf(':');
+                    rtReceiveMessage.AppendText(_receivestr + "\n");
+                    if (start >= 0 && start < rtReceiveMessage.TextLength && length > 0 && length + start < rtReceiveMessage.TextLength)
+                    {
+                        this.rtReceiveMessage.Select(start, length);
+                        this.rtReceiveMessage.SelectionColor = Color.Blue;
+                        this.rtReceiveMessage.SelectionLength = 0;
+                    }
+                }
+            }
+        }
+
+        private string GetName()
+        {
+            return Environment.MachineName;
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            IsThreadRunning = false;
+            String message = "Stop$" + GetName();
+            MulticastSend(MCASTADDR, MCASTPORT, message);
+        }
+    }
+}
